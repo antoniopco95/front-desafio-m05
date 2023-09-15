e12dafa6e555f4d7ced0e14ad0f5930ff63b39db
thisisnelsu <nupn@live.com>
2023-09-15 19:22:30 -0300

    Navegação entre Home, Clientes e Cobranças

--- front/src/pages/Dashboard/index.jsx
+++ front/src/pages/Dashboard/index.jsx
@@ -1,18 +1,48 @@
 import './styles.css'
 import VerticalHeader from '../../components/VerticalHeader'
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import AccountLogin from '../../components/AccountLogin';
 import HomeComponent from '../../components/HomeComponent';
+import ClientsComponent from '../../components/ClientsComponent';
 
 function Dashboard() {
 
+    const [home, setHome] = useState(true);
+    const [clients, setClients] = useState(false);
+    const [charges, setCharges] = useState(false);
+
+    const toggleHome = (e) => {
+        e.preventDefault();
+        setHome(true);
+        setClients(false);
+        setCharges(false);
+    };
+
+    const toggleClients = (e) => {
+        e.preventDefault();
+        setHome(false);
+        setClients(true);
+        setCharges(false);
+    };
+
+    const toggleCharges = (e) => {
+        e.preventDefault();
+        setHome(false);
+        setClients(false);
+        setCharges(true);
+    };
+
+    useEffect(() => {
+    }, [home, clients]);
+
     return (
         <div className='box-size'>
-            <VerticalHeader />
+            <VerticalHeader home={home} clients={clients} charges={charges} toggleHome={toggleHome} toggleClients={toggleClients} toggleCharges={toggleCharges} />
             <div className='login-header'>
                 <AccountLogin />
             </div>
-            <HomeComponent />
+            {home && <HomeComponent />}
+            {clients && <ClientsComponent />}
         </div>
     )
 };
