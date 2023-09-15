18077e37cb05c38b8b600cf3baa00d5a4afda79e
thisisnelsu <nupn@live.com>
2023-09-15 19:23:31 -0300

    Página de Clientes criada

---
+++ front/src/components/ClientsComponent/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+
+function ClientsComponent() {
+
+    return (
+        <div className='clientscomponent-box'>
+            Hello World
+        </div>
+    )
+};
+
+export default ClientsComponent;
