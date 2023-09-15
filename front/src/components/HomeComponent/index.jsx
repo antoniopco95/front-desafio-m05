d053eba37975cf19aefc8ba6b0549f506d7b9363
thisisnelsu <nupn@live.com>
2023-09-15 19:23:08 -0300

    Componente ClientsCard criado

--- front/src/components/HomeComponent/index.jsx
+++ front/src/components/HomeComponent/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ChargesCard from '../../components/ChargesCard'
+import ClientsCard from '../ClientsCard';
 import './styles.css'
+import PersonAdd from '../../assets/PersonAdd.svg'
+import PersonRemove from '../../assets/PersonRemove.svg'
 
 function HomeComponent() {
 
     return (
-        <div className='homecomponent-box'>
-            <ChargesCard chargesName='Vencidas' chargesNumber='08' chargesColor='red' />
-            <ChargesCard chargesName='Previstas' chargesNumber='05' chargesColor='yellow' />
-            <ChargesCard chargesName='Pagas' chargesNumber='10' chargesColor='blue' />
-        </div>
+        <>
+            <div className='homecard-box'>
+                <ChargesCard chargesName='Vencidas' chargesNumber='08' chargesColor='red' />
+                <ChargesCard chargesName='Previstas' chargesNumber='05' chargesColor='yellow' />
+                <ChargesCard chargesName='Pagas' chargesNumber='10' chargesColor='blue' />
+            </div>
+            <div className='clientscard-box'>
+                <ClientsCard clientsName='Inadimplentes' clientsNumber='08' clientsColor='red' iconChoose={PersonRemove} />
+                <ClientsCard clientsName='em dia' clientsNumber='08' clientsColor='blue' iconChoose={PersonAdd} />
+            </div>
+        </>
     )
 };
 
