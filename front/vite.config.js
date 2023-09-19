e5facac9787a76dd2ad9bc35b68e4a93f77d94b4
Matheus Lucas Ferreira <Matheuslucasepg@gmail.com>
2023-09-19 20:27:34 -0300

    base

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base:'/',
   plugins: [react()],
-})
+  build: {
+
+    outDir: 'dist',
+
+
+    minify: true,
+
+
+    sourcemap: true,
+
+
+    rollupOptions: {
+      input: 'src/main.jsx',
+    },
+  },
+});
