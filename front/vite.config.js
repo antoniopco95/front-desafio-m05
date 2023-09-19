34bd459d976e82c22aabcc15bc92f9eb5bec74ab
dreisdev <dreis.dev@outlook.com>
2023-09-19 16:49:32 -0300

    Deploy

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+
+    outDir: 'dist',
+
+
+    minify: true,
+
+
+    sourcemap: true,
+
+
+    rollupOptions: {
+      input: 'src/index.js',
+    },
+  },
+});
