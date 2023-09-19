66ce58a29218e0679a46d5cc2afbeb98fe331ed2
dreisdev <dreis.dev@outlook.com>
2023-09-19 07:21:26 -0300

    finalization pages SignUp and Login

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SignUp from './pages/SignUp/SignUp.jsx';
+import LoginUser from './pages/LoginUser/LoginUser.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+
+      {
+        path: "/register",
+        element: <SignUp />
+      },
+      {
+        path: "/login",
+        element: <LoginUser />
+      },
+
+    ],
+
+  },
+
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
