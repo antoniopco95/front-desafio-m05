3629b931911738e566250bddf92a57708f92da18
Matheus Lucas Ferreira <Matheuslucasepg@gmail.com>
2023-09-19 21:07:20 -0300

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 import './App.css'
 import 'react-toastify/dist/ReactToastify.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify'
 
@@ -19,6 +19,7 @@ function App() {
 
     <div>
       <ToastContainer />
+      <a onClick={useNavigate("/login")}>aaaaaaaa</a>
       <Outlet />
     </div>
 
