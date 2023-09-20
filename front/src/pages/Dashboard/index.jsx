427931007aaf1d4391be13d27fb1d3636a243914
antoniopco95 <antoniopco95@gmail.com>
2023-09-20 09:34:33 -0300

    navigate

--- front/src/pages/Dashboard/index.jsx
+++ front/src/pages/Dashboard/index.jsx
@@ -1,74 +1,85 @@
-import './styles.css'
-import VerticalHeader from '../../components/VerticalHeader'
-import React, { useState, useEffect } from 'react';
-import AccountLogin from '../../components/AccountLogin';
-import HomeComponent from '../../components/HomeComponent';
-import ClientsComponent from '../../components/ClientsComponent';
-import EditModal from '../../components/EditModal';
+import "./styles.css";
+import VerticalHeader from "../../components/VerticalHeader";
+import React, { useState, useEffect } from "react";
+import AccountLogin from "../../components/AccountLogin";
+import HomeComponent from "../../components/HomeComponent";
+import ClientsComponent from "../../components/ClientsComponent";
+import EditModal from "../../components/EditModal";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
-    
+  const [home, setHome] = useState(true);
+  const [clients, setClients] = useState(false);
+  const [charges, setCharges] = useState(false);
 
-    const [home, setHome] = useState(true);
-    const [clients, setClients] = useState(false);
-    const [charges, setCharges] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+  const handleOpenEdit = () => setOpenEdit(true);
+  const handleCloseEdit = () => setOpenEdit(false);
 
-    const [openEdit, setOpenEdit] = useState(false);
-    const handleOpenEdit = () => setOpenEdit(true);
-    const handleCloseEdit = () => setOpenEdit(false);
+  const [openAdd, setOpenAdd] = useState(false);
+  const handleOpenAdd = () => setOpenAdd(true);
+  const handleCloseAdd = () => setOpenAdd(false);
+  const [isAuthenticated, setIsAuthenticated] = React.useState(true);
+  const navigate = useNavigate();
 
-    const [openAdd, setOpenAdd] = useState(false);
-    const handleOpenAdd = () => setOpenAdd(true);
-    const handleCloseAdd = () => setOpenAdd(false);
-    const [isAuthenticated, setIsAuthenticated] = React.useState(true);
-    const navigate = useNavigate()
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setIsAuthenticated(false);
+    }
+  }, []);
 
+  if (!isAuthenticated) {
+    return navigate("/");
+  }
 
-    React.useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            setIsAuthenticated(false);
-        }
-    }, []);
+  const toggleHome = (e) => {
+    e.preventDefault();
+    setHome(true);
+    setClients(false);
+    setCharges(false);
+  };
 
-    if (!isAuthenticated) {
-        return navigate('/');}
+  const toggleClients = (e) => {
+    e.preventDefault();
+    setHome(false);
+    setClients(true);
+    setCharges(false);
+  };
 
-    const toggleHome = (e) => {
-        e.preventDefault();
-        setHome(true);
-        setClients(false);
-        setCharges(false);
-    };
+  const toggleCharges = (e) => {
+    e.preventDefault();
+    setHome(false);
+    setClients(false);
+    setCharges(true);
+  };
 
-    const toggleClients = (e) => {
-        e.preventDefault();
-        setHome(false);
-        setClients(true);
-        setCharges(false);
-    };
+  useEffect(() => {}, [home, clients]);
 
-    const toggleCharges = (e) => {
-        e.preventDefault();
-        setHome(false);
-        setClients(false);
-        setCharges(true);
-    };
+  return (
+    <div className="box-size">
+      <VerticalHeader
+        home={home}
+        clients={clients}
+        charges={charges}
+        toggleHome={toggleHome}
+        toggleClients={toggleClients}
+        toggleCharges={toggleCharges}
+      />
+      <div className="login-header">
+        <AccountLogin handleOpenEdit={handleOpenEdit} />
+      </div>
+      {home && <HomeComponent />}
+      {clients && (
+        <ClientsComponent
+          openAdd={openAdd}
+          handleOpenAdd={handleOpenAdd}
+          handleCloseAdd={handleCloseAdd}
+        />
+      )}
+      <EditModal openEdit={openEdit} handleCloseEdit={handleCloseEdit} />
+    </div>
+  );
+}
 
-    useEffect(() => {
-    }, [home, clients]);
-
-    return (
-        <div className='box-size'>
-            <VerticalHeader home={home} clients={clients} charges={charges} toggleHome={toggleHome} toggleClients={toggleClients} toggleCharges={toggleCharges} />
-            <div className='login-header'>
-                <AccountLogin handleOpenEdit={handleOpenEdit} />
-            </div>
-            {home && <HomeComponent />}
-            {clients && <ClientsComponent openAdd={openAdd} handleOpenAdd={handleOpenAdd} handleCloseAdd={handleCloseAdd} />}
-            <EditModal openEdit={openEdit} handleCloseEdit={handleCloseEdit} />
-        </div>
-    )
-};
-
-export default Dashboard
+export default Dashboard;
