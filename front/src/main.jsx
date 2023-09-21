16f66e627aadcd8e1eb44834388a9264dcaa1ba7
antoniopco95 <antoniopco95@gmail.com>
2023-09-21 15:09:20 -0300

    ajustes main

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,31 +5,29 @@ import SignUp from "./pages/SignUp/SignUp.jsx";
 import LoginUser from "./pages/LoginUser/LoginUser.jsx";
 import Dashboard from "./pages/Dashboard/index.jsx";
 import { ToastContainer } from "react-toastify";
-
 import ProtectedRoute from "./routes/protectedRoute.jsx";
-
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { UserProvider } from "./context/userContext";
-
 import ProtectedRoute from "./routes/protectedRoute.jsx";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginUser />} />
-        <Route path="/register" element={<SignUp />} />
-
-        <Route
-          path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
-
-      </Routes>
-    </Router>
-    <ToastContainer />
-  </React.StrictMode>
+  <StrictMode>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginUser />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route
+            path="/dashboard"
+            element={<ProtectedRoute element={<Dashboard />} />}
+          />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </UserProvider>
+  </StrictMode>
 );
